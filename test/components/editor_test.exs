a17141645b83dfac98a65ff01d66be3eb91113ca
ivanminutillo <ivanminutillo@outlook.it>
2022-05-20 19:40:07 +0200

    tests

---
+++ test/components/editor_test.exs
@@ -0,0 +1,67 @@
+defmodule Bonfire.Editor.Quil.EditorTests do
+  use Bonfire.Social.DataCase, async: true
+
+  alias Bonfire.Social.{Follows, FeedActivities}
+  alias Bonfire.Me.Fake
+
+
+  test "can publish a post" do
+    
+  end
+
+  test "can publish a post with emoji" do
+      
+  end
+
+  test "can mention a user" do
+      
+  end
+
+  test "can reply to an existing post from feed" do
+    
+  end
+
+  test "can reply to an existing post from thread" do
+    
+  end
+
+  test "When I reply to a post from the thread page, I see the new reply without refresh " do
+    
+  end
+
+  test "When I reply to a post from the feed, I see the new reply  " do
+    
+  end
+
+  test "When I publish a post, I see the live push that the post is published" do
+  
+  end
+
+  test "When I publish a post, the editor returns empty" do
+  
+  end
+
+  test "If I am replying to a post, the editor should mention automatically the other thread participants" do
+  
+  end
+
+  test "if I am replying to a post, I want to see the post above the editor" do
+  
+  end
+
+  test "If I click on the CW button, I want to see the related input" do
+  
+  end
+
+  test "If I publish a post with a CW, I want to see only the title in the feed" do
+    
+  end
+
+  test "upload a jpeg in the editor works" do
+    
+  end
+
+  test "add a caption to the image works" do
+    
+  end
+end
