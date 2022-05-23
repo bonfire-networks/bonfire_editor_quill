5dfd7221b2c16c9e0cd12a0b643b5d2e5905f504
Mayel de Borniol <git@mayel.space>
2022-05-23 21:30:02 +1200

    Fix CI

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,5 @@ ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Bonfire.Common.Config.get!(:repo_module), :manual)
 
 Application.put_env(:wallaby, :base_url, Bonfire.Web.Endpoint.url())
-{:ok, _} = Application.ensure_all_started(:wallaby)
+
+if File.exists?(Bonfire.Common.Config.get([:wallaby, :chromedriver, :path])), do: {:ok, _} = Application.ensure_all_started(:wallaby)
