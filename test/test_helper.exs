3a5755f971bd73701609fb6d1805ef1c8480401c
Mayel de Borniol <git@mayel.space>
2022-06-20 15:47:43 +1200

    Exclude tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-ExUnit.start()
+ExUnit.start(exclude: Bonfire.Common.RuntimeConfig.skip_test_tags())
 Ecto.Adapters.SQL.Sandbox.mode(Bonfire.Common.Config.get!(:repo_module), :manual)
 
 Application.put_env(:wallaby, :base_url, Bonfire.Web.Endpoint.url())
