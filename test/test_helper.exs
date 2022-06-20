dd7fdde5b9e575341116be55f059b489354376b1
Mayel de Borniol <git@mayel.space>
2022-06-20 14:21:46 +1200

    fix

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,4 +3,6 @@ Ecto.Adapters.SQL.Sandbox.mode(Bonfire.Common.Config.get!(:repo_module), :manual
 
 Application.put_env(:wallaby, :base_url, Bonfire.Web.Endpoint.url())
 
-if File.exists?(Bonfire.Common.Config.get([:wallaby, :chromedriver, :path])), do: {:ok, _} = Application.ensure_all_started(:wallaby)
+chromedriver_path = Bonfire.Common.Config.get([:wallaby, :chromedriver, :path])
+# TODO: skip browser-based tests if no driver is available
+if chromedriver_path && File.exists?(chromedriver_path), do: {:ok, _} = Application.ensure_all_started(:wallaby), else: IO.inspect("Note: Wallaby will not run because the chromedriver is missing")
