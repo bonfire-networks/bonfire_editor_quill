61c0940d1e1992123121c25f52560fecc407920b
Mayel de Borniol <git@mayel.space>
2022-07-24 14:39:14 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/426

--- lib/web/quill.ex
+++ lib/web/quill.ex
@@ -1,14 +1,14 @@
 defmodule Bonfire.Editor.Quill do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop field_name, :string
-  prop placeholder, :string
-  prop content, :string
+  prop field_name, :string, default: "html_body"
+  prop placeholder, :string, default: ""
+  prop content, :string, default: ""
   prop smart_input_text, :string, default: ""
-  prop insert_text, :any
-  prop textarea_class, :css_class
-  prop thread_mode, :string
-  prop showing_within, :string
+  prop insert_text, :string, default: ""
+  prop textarea_class, :css_class, default: nil
+  prop thread_mode, :any, default: nil
+  prop showing_within, :any, default: nil
 
   def output_format, do: :html # needed by apps to use this editor to know how to process text they receive from it
 
