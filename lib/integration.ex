097db63d623ea09b482014625b199d79a010df22
Mayel de Borniol <git@mayel.space>
2023-02-17 17:09:58 +1300

    Fix warnings

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.Editor.Quill.Integration do
   alias Bonfire.Common.Config
-  alias Bonfire.Common.Utils
-  import Untangle
+  # alias Bonfire.Common.Utils
+  # import Untangle
 
   def repo, do: Config.repo()
 end
