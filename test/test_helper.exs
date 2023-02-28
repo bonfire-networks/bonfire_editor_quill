8c0fcaf4e30025cfc785fa56b00840e939c06292
Mayel de Borniol <git@mayel.space>
2023-02-28 16:40:42 +1300

    d

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -11,4 +11,4 @@ chromedriver_path = Bonfire.Common.Config.get([:wallaby, :chromedriver, :path])
 # TODO: skip browser-based tests if no driver is available
 if chromedriver_path && File.exists?(chromedriver_path),
   do: {:ok, _} = Application.ensure_all_started(:wallaby),
-  else: IO.inspect("Note: Wallaby will not run because the chromedriver is missing")
+  else: IO.inspect("Note: Wallaby UI tests will not run because the chromedriver is missing")
