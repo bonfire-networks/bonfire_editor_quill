d190d19214c31d27ac803d2d6e6b335e86506c8c
Mayel de Borniol <git@mayel.space>
2023-12-16 14:48:23 +0000

    compilers

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Bonfire.Editor.Quill.MixProject do
       version: "0.0.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: Mix.compilers(),
+      compilers: Mix.compilers() ++ [:surface],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps:
