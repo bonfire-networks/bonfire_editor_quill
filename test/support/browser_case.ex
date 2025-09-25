23d1436090d96cdd8a511217f9eaf333e0f15023
Mayel de Borniol <git@mayel.space>
2025-09-25 13:27:21 +0100

    tests

--- test/support/browser_case.ex
+++ test/support/browser_case.ex
@@ -32,6 +32,46 @@ defmodule Bonfire.Editor.Quill.BrowserCase do
       setup _ do
         on_exit(fn -> Application.put_env(:wallaby, :js_logger, :stdio) end)
       end
+
+      def user_browser_session(session) do
+        username = System.get_env("ADMIN_USER", "test_user")
+        pw = System.get_env("ADMIN_PASSWORD", "for-testing-only")
+        account = fake_account!(%{credential: %{password: pw}})
+
+        user =
+          fake_user!(account, %{
+            username: username,
+            name: username
+          })
+
+        # alice = fake_user!()
+        # conn = conn(user: alice)
+        # conn = Phoenix.ConnTest.get(conn, "/")
+        # %{@cookie_key => %{value: token}} = conn.resp_cookies
+
+        user_session =
+          session
+          |> visit("/login")
+          |> fill_in(Query.fillable_field("login_fields[email_or_username]"),
+            with: username
+          )
+          |> fill_in(Query.fillable_field("login_fields[password]"), with: pw)
+          # |> Browser.send_keys([:enter])
+          |> click(Query.button("Log in"))
+
+        # |> Browser.set_cookie(@cookie_key, token)
+      end
+
+      def new_post(session, text \\ "Hello world") do
+        session
+        # |> visit("/feed")
+        # |> fill_in(text_field("#editor_quill .ql-editor"), with: text)
+        |> click(Query.css("#editor .ql-editor"))
+        |> Browser.send_keys([text])
+        |> click(Query.button("Post"))
+        # |> assert_has(css(".alert", text: "Posted!"))
+        |> assert_text(Query.css("article"), text)
+      end
     end
   end
 
